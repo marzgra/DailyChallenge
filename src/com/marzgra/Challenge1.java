f9a8f92148e48535997307a3461c0929afe9e195
marzgra <marzgra@o2.pl>
2020-01-29 20:02:54 +0100

    DailyChallenge #1

---
+++ src/com/marzgra/Challenge1.java
@@ -0,0 +1,27 @@
+package com.marzgra;
+
+public class Challenge1 {
+    /*
+    Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+    For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+    Bonus: Can you do this in one pass?
+     */
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 15, 3, 7};
+        int sum = 10;
+        System.out.println(doesAnyTwoNumbersSumUpTo(numbers, sum));
+    }
+
+    private static boolean doesAnyTwoNumbersSumUpTo(int[] numbers, int sum) {
+        for(int i = 0; i < numbers.length; i++) {
+            for (int j = i; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == sum) {
+                    System.out.println(numbers[i] + " + " + numbers[j] + " = " + sum);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
